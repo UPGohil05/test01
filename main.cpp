bd9cba4e073c9356c9c0f9bb3d02e3e7205b3fa6
Uttam Gohil <itsjupiter000@gmail.com>
2024-08-06 23:18:59 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>arr={1, 2, 3, 4, 6, 6,6 , 8, 8, 9};
+    priority_queue<int> pq;
+    //count the frequency of each element and add it to the priority queue without map
+    for(int i=0;i<arr.size();i++){
+        pq.push(arr[i]);
+    }
+    //print the frequency of each element
+    while(!pq.empty()){
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+
+}
